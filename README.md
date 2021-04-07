308936ddad780ff3398f31a1bb3c164dbc153ae2
qbixmex <qbixmex@gmail.com>
2021-04-07 04:21:34 -0500

    Todos App Init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # React Hooks Tutorial
 
-## Versión: `1.0.18`
+## Versión: `1.0.19`
 
 ## Scripts Disponibles
 
--- src/components/09-use-reducer/TodoApp.jsx
+++ src/components/09-use-reducer/TodoApp.jsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import TodoReducer from './todoReducer';
+import "./TodoApp.css"
 
 const initialState = [{
   id: new Date().getTime(),
@@ -15,13 +16,45 @@ const TodoApp = () => {
 
   return (
     <section>
-      <h2>Todo App</h2>
+      <h2 className="text-center mb-4">Todo App <small>({ todos.length })</small></h2>
 
-      <ol>
-        {
-          todos.map(todo => (<li key={todo.id}>{ todo.description }</li>))
-        }
-      </ol>
+      <div className="mb-5">
+        <form>
+          <div className="form-group">
+            <input
+              name="description"
+              type="text"
+              className="form-control"
+              autoComplete="off"
+            />
+          </div>
+        </form>
+      </div>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th className="text-center">Done</th>
+            <th className="text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todos.map((todo, i) => (
+              <tr key={todo.id}>
+                <td>
+                  <p className="description">{ `${i + 1}. ${todo.description}` }</p>
+                </td>
+                <td className="text-center">{ todo.done ? "Yes" : "No" }</td>
+                <td className="text-center">
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </section>
   );
 }
